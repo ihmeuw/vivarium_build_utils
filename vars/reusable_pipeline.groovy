cf6af5cdf9d9dd353c5a199aeaac641da4e66f43
patricktnast <130876799+patricktnast@users.noreply.github.com>
2025-01-06 15:30:32 -0800

    Remove matrix from reusable pipeline (#15)
    * swap to parallel
    
    * adjust organization
    
    * one more try
    
    * change name
    
    * change signature of scheduled branches
    
    * remove checkout scm
    
    * try moving sh commands
    
    * add back checkout scm
    
    * stringify
    
    * add echo
    
    * add another print statement
    
    * don't use variable prematurely
    
    * try defining in withEnv
    
    * checkout scm sooner
    
    * what happens if we just remove envvars
    
    * try removing steps
    
    * add back checkout scm
    
    * fix env var cap
    
    * add build and deploiy
    
    * add back braces
    
    * ok fine checkout scm
    
    * try adding the tests
    
    * dedupe names
    
    * add test types
    
    * add brace
    
    * try to add slurm nodes
    
    * add python version to stage names

--- vars/reusable_pipeline.groovy
+++ vars/reusable_pipeline.groovy
@@ -3,8 +3,22 @@ def call(Map config = [:]){
   Example: fhs_standard_pipeline(job_name: JOB_NAME)
   JOB_NAME is a reserved Jenkins var
   */
-  scheduled_branches = config.scheduled_branches ?: '' 
-  CRON_SETTINGS = scheduled_branches.split(',').collect{it.trim()}.contains(BRANCH_NAME) ? 'H H(20-23) * * *' : ''
+  requires_slurm = config.requires_slurm ?: false
+  task_node = requires_slurm ? 'slurm' : 'matrix-tasks'
+
+  scheduled_branches = config.scheduled_branches ?: [] 
+  CRON_SETTINGS = scheduled_branches.contains(BRANCH_NAME) ? 'H H(20-23) * * *' : ''
+
+  // Define Python versions - can be parameterized through config
+  python_versions = config.python_versions ?: ["3.10", "3.11"]
+  PYTHON_DEPLOY_VERSION = "3.11"
+
+  test_types = config.test_types ?: []
+  // raise an error if test_types is not a subset of  ['e2e', 'unit', 'integration']
+  if (!test_types.every { ['e2e', 'unit', 'integration'].contains(it) }) {
+    throw new IllegalArgumentException("test_types must be a subset of ['e2e', 'unit', 'integration']")
+  }
+
   pipeline {
     // This agent runs as svc-simsci on node simsci-ci-coordinator-01.
     // It has access to standard IHME filesystems and singularity
@@ -44,6 +58,7 @@ def call(Map config = [:]){
       description: "Used as needed for debugging purposes."
       )
     }
+
     triggers {
       cron(CRON_SETTINGS)
     }
@@ -58,198 +73,175 @@ def call(Map config = [:]){
         }
       }
 
-      stage("Python version matrix") {
-      // we don't want to go to deployment if any of the matrix branches fail
-      failFast true
-        matrix {
-          // customWorkspace setting must be ran within a node
-          agent {
-            node {
-              label "matrix-tasks"
-            }
-          }
-          axes {
-            axis {
-              // parallelize by python minor version
-              name 'PYTHON_VERSION'
-              values "3.10", "3.11"
-            }
-          }
+      stage("Python Versions") {
+        steps {
+          script {
+            def parallelPythonVersions = [:]
+            
+            python_versions.each { pythonVersion ->
+              parallelPythonVersions["Python ${pythonVersion}"] = {
+                node(task_node) {
+                  def envVars = [
+                    CONDA_ENV_NAME: "${env.JOB_NAME}-${BUILD_NUMBER}-${pythonVersion}",
+                    CONDA_ENV_PATH: "/tmp/${env.JOB_NAME}-${BUILD_NUMBER}-${pythonVersion}",
+                    SHARED_PATH: "/svc-simsci",
+                    BRANCH: sh(script: "echo ${GIT_BRANCH} | rev | cut -d '/' -f1 | rev", returnStdout: true).trim(),
+                    TIMESTAMP: sh(script: 'date', returnStdout: true),
+                    CONDARC: "/svc-simsci/miniconda3/.condarc",
+                    CONDA_BIN_PATH: "/svc-simsci/miniconda3/bin",
+                    PYTHON_VERSION: pythonVersion,
+                    XDG_CACHE_HOME: "/svc-simsci/pip-cache",
+                    ACTIVATE: "source /svc-simsci/miniconda3/bin/activate /tmp/${env.JOB_NAME}-${BUILD_NUMBER}-${pythonVersion} &> /dev/null",
+                    ACTIVATE_BASE: "source /svc-simsci/miniconda3/bin/activate &> /dev/null"
+                  ]
+                  
+                  withEnv(envVars.collect { k, v -> "${k}=${v}" }) {
+                    try {
+                      checkout scm
+                      stage("Debug Info - Python ${pythonVersion}") {
+                        echo "Jenkins pipeline run timestamp: ${TIMESTAMP}"
+                        // Display parameters used.
+                        echo """Parameters:
+                        DEPLOY_OVERRIDE: ${params.DEPLOY_OVERRIDE}"""
 
-          environment {
-            // pipeline_name=${config.pipeline_name}
-            conda_env_name="${env.JOB_NAME}-${BUILD_NUMBER}-${PYTHON_VERSION}"
-            conda_env_path="/tmp/${conda_env_name}"
-            // defaults for conda and pip are a local directory /svc-simsci for improved speed.
-            // In the past, we used /ihme/code/* on the NFS (which is slower)
-            shared_path="/svc-simsci"
-            // Get the branch being built and strip everything but the text after the last "/"
-            BRANCH = sh(script: "echo ${GIT_BRANCH} | rev | cut -d '/' -f1 | rev", returnStdout: true).trim()
-            TIMESTAMP = sh(script: 'date', returnStdout: true)
-            // Specify the path to the .condarc file via environment variable.
-            // This file configures the shared conda package cache.
-            CONDARC = "${shared_path}/miniconda3/.condarc"
-            CONDA_BIN_PATH = "${shared_path}/miniconda3/bin"
-            // Specify conda env by build number so that we don't have collisions if builds from
-            // different branches happen concurrently.
-            PYTHON_DEPLOY_VERSION = "3.11"
-            CONDA_ENV_NAME = "${conda_env_name}"
-            CONDA_ENV_PATH = "${conda_env_path}_${PYTHON_VERSION}"
-            // Set the Pip cache.
-            XDG_CACHE_HOME = "${shared_path}/pip-cache"
-            // Jenkins commands run in separate processes, so need to activate the environment every
-            // time we run pip, poetry, etc.
-            ACTIVATE = "source ${CONDA_BIN_PATH}/activate ${CONDA_ENV_PATH} &> /dev/null"
-          }
+                        // Display environment variables from Jenkins.
+                        echo """Environment:
+                        ACTIVATE:       '${ACTIVATE}'
+                        BUILD_NUMBER:   '${BUILD_NUMBER}'
+                        BRANCH:         '${BRANCH}'
+                        CONDARC:        '${CONDARC}'
+                        CONDA_BIN_PATH: '${CONDA_BIN_PATH}'
+                        CONDA_ENV_NAME: '${CONDA_ENV_NAME}'
+                        CONDA_ENV_PATH: '${CONDA_ENV_PATH}'
+                        GIT_BRANCH:     '${GIT_BRANCH}'
+                        JOB_NAME:       '${JOB_NAME}'
+                        WORKSPACE:      '${WORKSPACE}'
+                        XDG_CACHE_HOME: '${XDG_CACHE_HOME}'"""
+                      }
 
-          stages {
+                      stage("Build Environment - Python ${pythonVersion}") {
+                        // The env should have been cleaned out after the last build, but delete it again
+                        // here just to be safe.
+                        sh "rm -rf ${CONDA_ENV_PATH}"
+                        sh "${ACTIVATE_BASE} && make build-env PYTHON_VERSION=${PYTHON_VERSION}"
+                        // open permissions for test users to create file in workspace
+                        sh "chmod 777 ${WORKSPACE}"
+                      }
 
-            stage("Debug Info") {
-              steps {
-                echo "Jenkins pipeline run timestamp: ${TIMESTAMP}"
-                // Display parameters used.
-                echo """Parameters:
-                DEPLOY_OVERRIDE: ${params.DEPLOY_OVERRIDE}"""
+                      stage("Install Package - Python ${pythonVersion}") {
+                        sh "${ACTIVATE} && make install"
+                      }
 
-                // Display environment variables from Jenkins.
-                echo """Environment:
-                ACTIVATE:       '${ACTIVATE}'
-                BUILD_NUMBER:   '${BUILD_NUMBER}'
-                BRANCH:         '${BRANCH}'
-                CONDARC:        '${CONDARC}'
-                CONDA_BIN_PATH: '${CONDA_BIN_PATH}'
-                CONDA_ENV_NAME: '${CONDA_ENV_NAME}'
-                CONDA_ENV_PATH: '${CONDA_ENV_PATH}'
-                GIT_BRANCH:     '${GIT_BRANCH}'
-                JOB_NAME:       '${JOB_NAME}'
-                WORKSPACE:      '${WORKSPACE}'
-                XDG_CACHE_HOME: '${XDG_CACHE_HOME}'"""
-              }
-            }
+                      stage("Format - Python ${pythonVersion}") {
+                        sh "${ACTIVATE} && make format"
+                      }
 
-            stage("Build Environment") {
-              environment {
-                // Command for activating the base environment. Activating the base environment sets
-                // the correct path to the conda binary which is used to create a new conda env.
-                ACTIVATE_BASE = "source ${CONDA_BIN_PATH}/activate &> /dev/null"
-              }
-              steps {
-                // The env should have been cleaned out after the last build, but delete it again
-                // here just to be safe.
-                sh "rm -rf ${CONDA_ENV_PATH}"
-                sh "${ACTIVATE_BASE} && make build-env PYTHON_VERSION=${PYTHON_VERSION}"
-                // open permissions for test users to create file in workspace
-                sh "chmod 777 ${WORKSPACE}"
-              }
-            }
+                    stage("Run Tests - Python ${pythonVersion}") {
+                      script {
+                          def full_name = { test_type ->
+                            if (test_type == 'e2e') {
+                                return "End-to-End"
+                            } else {
+                                return test_type.capitalize()
+                            }
+                          }
+                          def parallelTests = test_types.collectEntries {
+                              ["${full_name(it)} Tests" : {
+                                  stage("Run ${full_name(it)} Tests - Python ${pythonVersion}") {
+                                      sh "${ACTIVATE} && make ${it}"
+                                      publishHTML([
+                                        allowMissing: true,
+                                        alwaysLinkToLastBuild: false,
+                                        keepAll: true,
+                                        reportDir: "output/htmlcov_${it}",
+                                        reportFiles: "index.html",
+                                        reportName: "Coverage Report - ${full_name(it)} tests",
+                                        reportTitles: ''
+                                      ])
+                                  }
+                              }]
+                          }
+                          parallel parallelTests
+                      }
+                    }
 
-            stage("Install Package") {
-              steps {
-                sh "${ACTIVATE} && make install"
-              }
-            }
-
-            stage("Format") {
-              steps {
-                sh "${ACTIVATE} && make format"
-              }
-            }
-
-            stage("Run Integration Tests") {
-              steps {
-                sh "${ACTIVATE} && make integration"
-                publishHTML([
-                  allowMissing: true,
-                  alwaysLinkToLastBuild: false,
-                  keepAll: true,
-                  reportDir: "output/htmlcov_integration",
-                  reportFiles: "index.html",
-                  reportName: "Coverage Report - Integration tests",
-                  reportTitles: ''
-                ])
-              }
-            }
-
-            stage('Build and Deploy') {
-              when {
-                expression { "${PYTHON_DEPLOY_VERSION}" == "${PYTHON_VERSION}" }
-              }
-              stages {
-                stage("Build Docs") {
-                  steps {
-                    sh "${ACTIVATE} && make build-doc"
-                  }
-                }
-                stage("Build Package") {
-                  steps {
-                    sh "${ACTIVATE} && make build-package"
+                    if (PYTHON_VERSION == PYTHON_DEPLOY_VERSION) {
+                        stage("Build and Deploy - Python ${pythonVersion}") {
+                          stage("Build Docs - Python ${pythonVersion}") {
+                            sh "${ACTIVATE} && make build-doc"
+                          }
+                          stage("Build Package - Python ${pythonVersion}") {
+                            sh "${ACTIVATE} && make build-package"
+                          }
+                        }
+                      }
+                    } finally {
+                  // Cleanup
+                      sh "${ACTIVATE} && make clean"
+                      sh "rm -rf ${conda_env_path}"
+                      cleanWs()
+                      dir("${WORKSPACE}@tmp") {
+                        deleteDir()
+                      }
+                    }
                   }
                 }
               }
             }
-          } // stages within python version matrix
 
-          post {
-            always {
-              // Generate a message to send to Slack.
-              script {
-                // Run git command to get the author of the last commit
-                developerID = sh(
-                  script: "git log -1 --pretty=format:'%an'",
-                  returnStdout: true
-                ).trim()
-                echo "Most recent developerID: ${developerID}"
-                if (env.BRANCH == "main") {
-                  channelName = "simsci-ci-status"
-                  slackID = "channel"
-                } else {
-                  channelName = "simsci-ci-status-test"
-                  slackID = github_slack_mapper(github_author: developerID)
-                }
-                echo "slackID to tag in slack message: ${slackID}"
-                slackMessage = """
-                  Job: *${env.JOB_NAME}*
-                  Build number: #${env.BUILD_NUMBER}
-                  Build status: *${currentBuild.result}*
-                  Author: @${slackID}
-                  Build details: <${env.BUILD_URL}/console|See in web console>
-                  """.stripIndent()
-              }
-            }
-            failure {
-              echo "This build triggered by ${developerID} failed on ${GIT_BRANCH}. Sending a failure message to Slack."
-              slackSend channel: "#${channelName}",
-                        message: slackMessage,
-                        teamDomain: "ihme",
-                        tokenCredentialId: "slack"
-            }
-            success {
-              script {
-                if (params.DEBUG) {
-                  echo 'Debug is enabled. Sending a success message to Slack.'
-                  slackSend channel: "#${channelName}",
-                            message: slackMessage,
-                            teamDomain: "ihme",
-                            tokenCredentialId: "slack"
-                } else {
-                  echo 'Debug is not enabled. No success message will be sent to Slack.'
-                }
-              }
-            }
-            cleanup {  // cleanup for python matrix workspaces
-              sh "${ACTIVATE} && make clean"
-              sh "rm -rf ${CONDA_ENV_PATH}"
-              cleanWs()
-              // manually remove @tmp dirs
-              dir("${WORKSPACE}@tmp"){
-                deleteDir()
-              }
-            }
-          }  // End of post stage
-        }  // End of python version matrix
-      }  // End of python version matrix stage
-    }  // End of stages
+            parallel parallelPythonVersions
+          }
+        }
+      }
+    }
+
     post {
+      always {
+        // Generate a message to send to Slack.
+        script {
+          // Run git command to get the author of the last commit
+          developerID = sh(
+            script: "git log -1 --pretty=format:'%an'",
+            returnStdout: true
+          ).trim()
+          echo "Most recent developerID: ${developerID}"
+          if (env.BRANCH == "main") {
+            channelName = "simsci-ci-status"
+            slackID = "channel"
+          } else {
+            channelName = "simsci-ci-status-test"
+            slackID = github_slack_mapper(github_author: developerID)
+          }
+          echo "slackID to tag in slack message: ${slackID}"
+          slackMessage = """
+            Job: *${env.JOB_NAME}*
+            Build number: #${env.BUILD_NUMBER}
+            Build status: *${currentBuild.result}*
+            Author: @${slackID}
+            Build details: <${env.BUILD_URL}/console|See in web console>
+            """.stripIndent()
+        }
+      }
+      failure {
+        echo "This build triggered by ${developerID} failed on ${GIT_BRANCH}. Sending a failure message to Slack."
+        slackSend channel: "#${channelName}",
+                  message: slackMessage,
+                  teamDomain: "ihme",
+                  tokenCredentialId: "slack"
+      }
+      success {
+        script {
+          if (params.DEBUG) {
+            echo 'Debug is enabled. Sending a success message to Slack.'
+            slackSend channel: "#${channelName}",
+                      message: slackMessage,
+                      teamDomain: "ihme",
+                      tokenCredentialId: "slack"
+          } else {
+            echo 'Debug is not enabled. No success message will be sent to Slack.'
+          }
+        }
+      }
       cleanup { // cleanup for outer workspace
         cleanWs()
         // manually remove @tmp dirs
@@ -257,6 +249,6 @@ def call(Map config = [:]){
           deleteDir()
         }
       }
-    }
+    }  // End of post
   }  // End of pipeline
 }
