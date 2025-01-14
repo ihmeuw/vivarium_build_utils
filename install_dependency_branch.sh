d8b552cf394b3d7dac7f0c08399da740d6ecb33f
patricktnast <130876799+patricktnast@users.noreply.github.com>
2025-01-14 13:11:02 -0800

    try to remove checkouts

--- install_dependency_branch.sh
+++ install_dependency_branch.sh
@@ -7,39 +7,31 @@ workflow=$3
 
 root_dir=$(pwd)
 dependency_branch_name='main'
-branch_name_to_check=${branch_name}
+branch_name_to_check=$(git rev-parse --abbrev-ref HEAD)
 iterations=0
 
 while [ "$branch_name_to_check" != "$dependency_branch_name" ] && [ $iterations -lt 20 ]
 do
   echo "Checking for ${dependency_name} branch: '${branch_name_to_check}'"
-  if
-    git ls-remote --exit-code \
-    --heads https://github.com/ihmeuw/"${dependency_name}".git "${branch_name_to_check}" == "0"
+  if git ls-remote --exit-code --heads https://github.com/ihmeuw/"${dependency_name}".git "${branch_name_to_check}" == "0"
   then
     dependency_branch_name=${branch_name_to_check}
     echo "Found matching branch: ${dependency_branch_name}"
   else
     echo "Could not find ${dependency_name} branch '${branch_name_to_check}'. Finding parent branch."
-    branch_name_to_check="$( \
-      git show-branch -a \
-      | grep '\*' \
-      | grep -v "${branch_name_to_check}" \
-      | head -n1 \
-      | sed 's/[^\[]*//' \
-      | awk 'match($0, /\[[a-zA-Z0-9\/.-]+\]/) { print substr( $0, RSTART+1, RLENGTH-2 )}' \
-      | sed 's/^origin\///' \
-    )"
-    if [ -z "$branch_name_to_check" ]; then
+    # Get the commit hash of where this branch diverged from main
+    merge_base=$(git merge-base "${branch_name_to_check}" main)
+    # Find the next parent branch by getting the symbolic name of the merge base
+    branch_name_to_check=$(git name-rev --exclude "tags/*" --exclude "${branch_name_to_check}" --refs="refs/heads/*" --name-only "${merge_base}" | sed 's/\^[0-9]*$//')
+    
+    if [ -z "$branch_name_to_check" ] || [ "$branch_name_to_check" = "undefined" ]; then
       echo "Could not find parent branch. Will use released version of ${dependency_name}."
       branch_name_to_check="main"
     fi
-    echo "Checking out branch: ${branch_name_to_check}"
-    git checkout "${branch_name_to_check}"
+    echo "Next branch to check: ${branch_name_to_check}"
     iterations=$((iterations+1))
   fi
 done
-git checkout "${branch_name}"
 
 if [ "$workflow" == "github" ]; then
   echo "${dependency_name}_branch_name=${dependency_branch_name}" >> "$GITHUB_ENV"
