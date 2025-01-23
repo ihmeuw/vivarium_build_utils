e17bd49011c657eb587d4365e96c62f523270434
Patrick Nast <pnast@uw.edu>
2025-01-23 15:04:13 -0800

    add all tests to reusable pipeline

--- vars/reusable_pipeline.groovy
+++ vars/reusable_pipeline.groovy
@@ -23,8 +23,8 @@ def call(Map config = [:]){
 
   test_types = config.test_types ?: []
   // raise an error if test_types is not a subset of  ['e2e', 'unit', 'integration']
-  if (!test_types.every { ['e2e', 'unit', 'integration'].contains(it) }) {
-    throw new IllegalArgumentException("test_types must be a subset of ['e2e', 'unit', 'integration']")
+  if (!test_types.every { ['all-tests', 'e2e', 'unit', 'integration'].contains(it) }) {
+    throw new IllegalArgumentException("test_types must be a subset of ['all-tests', 'e2e', 'unit', 'integration']")
   }
   // Allow for building conda env on shared fs if required
   conda_env_name = config.use_shared_fs ? "${env.JOB_NAME.replaceAll('/', '-')}-${BUILD_NUMBER}" : "${env.JOB_NAME}-${BUILD_NUMBER}"
@@ -154,9 +154,11 @@ def call(Map config = [:]){
                           def full_name = { test_type ->
                             if (test_type == 'e2e') {
                                 return "End-to-End"
+                            } else if (test_type == 'all-tests') {
+                              return "All Tests"
                             } else {
                                 return test_type.capitalize()
-                            }
+                              }
                           }
                           def parallelTests = test_types.collectEntries {
                               ["${full_name(it)} Tests" : {
