42306719dc9b9a305761bbc2444456dd2dc511ed
Patrick Nast <pnast@uw.edu>
2025-01-23 14:41:32 -0800

    add description to tests

--- resources/makefiles/test.mk
+++ resources/makefiles/test.mk
@@ -8,20 +8,20 @@ define test-runslow
 	pytest -vvv --runslow --cov --cov-report term --cov-report html:./output/htmlcov_$(1) tests/$(1)/
 endef
 
-e2e: $(MAKE_SOURCES)
+e2e: $(MAKE_SOURCES) # Run end-to-end tests
 	$(call test,e2e)
 
-e2e-runslow: $(MAKE_SOURCES)
+e2e-runslow: $(MAKE_SOURCES) # Run end-to-end tests with slow tests
 	$(call test-runslow,e2e)
 
-integration: $(MAKE_SOURCES)
+integration: $(MAKE_SOURCES) # Run integration tests
 	$(call test,integration)
 
-integration-runslow: $(MAKE_SOURCES)
+integration-runslow: $(MAKE_SOURCES) # Run integration tests with slow tests
 	$(call test-runslow,integration)
 
-unit: $(MAKE_SOURCES)
+unit: $(MAKE_SOURCES) # Run unit tests
 	$(call test,unit)
 
-unit-runslow: $(MAKE_SOURCES)
+unit-runslow: $(MAKE_SOURCES) # Run unit tests with slow tests
 	$(call test-runslow,unit)
