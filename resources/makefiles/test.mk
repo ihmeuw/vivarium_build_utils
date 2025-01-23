9b0cb9860c1def13daac3f276e7a745080783249
Patrick Nast <pnast@uw.edu>
2025-01-23 14:59:22 -0800

    add all-test, consolidate

--- resources/makefiles/test.mk
+++ resources/makefiles/test.mk
@@ -1,27 +1,17 @@
 define test
 	export COVERAGE_FILE=./output/.coverage.$(1) && \
-	pytest -vvv --cov --cov-report term --cov-report html:./output/htmlcov_$(1) tests/$(1)/
+	pytest -vvv $(if $(RUNSLOW),--runslow,) --cov --cov-report term --cov-report html:./output/htmlcov_$(1) tests/$(1)
 endef
 
-define test-runslow
-	export COVERAGE_FILE=./output/.coverage.$(1) && \
-	pytest -vvv --runslow --cov --cov-report term --cov-report html:./output/htmlcov_$(1) tests/$(1)/
-endef
+all-tests: $(MAKE_SOURCES) # Run all tests
+	export COVERAGE_FILE=./output/.coverage && \
+	pytest -vvv $(if $(RUNSLOW),--runslow,) --cov --cov-report term --cov-report html:./output/htmlcov_tests tests/
 
 e2e: $(MAKE_SOURCES) # Run end-to-end tests
 	$(call test,e2e)
 
-e2e-runslow: $(MAKE_SOURCES) # Run end-to-end tests with slow tests
-	$(call test-runslow,e2e)
-
 integration: $(MAKE_SOURCES) # Run integration tests
 	$(call test,integration)
 
-integration-runslow: $(MAKE_SOURCES) # Run integration tests with slow tests
-	$(call test-runslow,integration)
-
 unit: $(MAKE_SOURCES) # Run unit tests
 	$(call test,unit)
-
-unit-runslow: $(MAKE_SOURCES) # Run unit tests with slow tests
-	$(call test-runslow,unit)
