089481d3cd9f5853a366529d928b48e01312605c
Patrick Nast <pnast@uw.edu>
2025-01-30 13:46:43 -0800

    handle the file differently

--- vars/get_python_versions.groovy
+++ vars/get_python_versions.groovy
@@ -3,11 +3,6 @@
 import groovy.json.JsonSlurper
 
 def call() {
-    def python_versions = []
-    def python_versions_file = readFile("python_versions.json")
-    def python_versions_json = new JsonSlurper().parseText(python_versions_file)
-    python_versions_json.each { version ->
-        python_versions.add(version)
-    }
-    return python_versions
+    def python_versions_file = new File("python_versions.json")
+    return new JsonSlurper().parse(python_versions_file)
 }
