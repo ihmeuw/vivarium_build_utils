c584f1f9b970beb7b4e194fadfa0b3ff59da0afe
Patrick Nast <pnast@uw.edu>
2025-02-05 10:37:55 -0800

    throw a good error if the file doesn't exist

--- vars/get_python_versions.groovy
+++ vars/get_python_versions.groovy
@@ -3,6 +3,11 @@
 import groovy.json.JsonSlurper
 
 def call(workspace) {
-    def python_versions_string = readFile "${workspace}/python_versions.json"
+    // raise an error if the file doesn't exist
+    def filename = "${workspace}/python_versions.json"
+    if (!fileExists(filename)) {
+        error("python_versions.json file not found in repository under test.")
+    }
+    def python_versions_string = readFile filename
     return new JsonSlurper().parseText(python_versions_string)
 }
