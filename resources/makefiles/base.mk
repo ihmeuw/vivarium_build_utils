378845a3b6d05f4d67572ff9c66dcc464598ec2e
Steve Bachmeier <sbachmei@uw.edu>
2025-07-22 16:11:17 -0700

    update comments

--- resources/makefiles/base.mk
+++ resources/makefiles/base.mk
@@ -31,8 +31,10 @@ MAKE_SOURCES := $(shell find . -type d -name "*" ! -path "./.git*" ! -path "./.v
 # Help and debugging commands #
 ###############################
 
-# List of Make targets is generated dynamically. To add description of target, use a # on the target definition.
-list:
+# List of Make targets is generated dynamically. Note that to have a target show up in this
+# list, it must have an in-line comment starting with a '#' on the target definition,
+# e.g. some-target:  # this is the description for some-target
+list: # Print available Make targets
 	@echo
 	@echo "Make targets:"
 	@for file in Makefile $(UTILS_DIR)resources/makefiles/*.mk; do \
@@ -40,7 +42,7 @@ list:
 	done | column -t -s:
 	@echo
 
-debug: # Print debug information (environment variables)
+debug: # Print debug information
 	@echo "'make' invoked with these environment variables:"
 	@echo "CONDA_ENV_NAME:                   ${CONDA_ENV_NAME}"
 	@echo "IHME_PYPI:                        ${IHME_PYPI}"
@@ -56,36 +58,37 @@ debug: # Print debug information (environment variables)
 # Jenkins build commands #
 ##########################
 
-create-env: ## Create a new conda environment. Specify env name with CONDA_ENV_NAME. Default env name is PACKAGE_NAME_PYTHON_VERSION.
+create-env: # Create a new conda environment with name {PACKAGE_NAME}_py{PYTHON_VERSION}.
 	conda create ${CONDA_ENV_CREATION_FLAG} python=${PYTHON_VERSION} --yes
 
 install: ENV_REQS?=dev
-install: ## Install setuptools, package, and build utilities
+install: # Install package and dependencies
 	pip install uv
 	uv pip install --upgrade pip setuptools 
 	uv pip install -e .[${ENV_REQS}] --extra-index-url ${IHME_PYPI}simple/ --index-strategy unsafe-best-match
 
 lint: # Check for formatting errors
+# NOTE: This is not precisely 'linting' but we historically use that term here
 	isort $(LOCATIONS) --check --verbose --only-modified --diff
 	black $(LOCATIONS) --check --diff
 
-mypy: # Check for type hinting erros
+mypy: # Check for type hinting errors
 	mypy --config-file pyproject.toml .
 
 # test: test commands are defined in test.mk
 
-build-doc: $(MAKE_SOURCES) # Build the docs
+build-doc: $(MAKE_SOURCES) # Build documentation
 	$(MAKE) -C docs/ html SPHINXOPTS="-T -W --keep-going"
 	@echo "Ignore, Created by Makefile, `date`" > $@
 
-test-doc: $(MAKE_SOURCES) # Test docs
+test-doc: $(MAKE_SOURCES) # Test documentation examples
 	$(MAKE) doctest -C docs/
 
-tag-version: # Tag the version and push
+tag-version: # Tag current version and push to git
 	git tag -a "v${PACKAGE_VERSION}" -m "Tag automatically generated from Jenkins."
 	git push --tags
 
-build-package: $(MAKE_SOURCES) # Build the package as a pip wheel
+build-package: $(MAKE_SOURCES) # Build pip wheel package
 	pip install build
 	python -m build
 	@echo "Ignore, Created by Makefile, `date`" > $@
@@ -96,14 +99,14 @@ deploy-package-artifactory: # Deploy the package to Artifactory
 	pip install twine
 	twine upload --repository-url ${IHME_PYPI} -u ${PYPI_ARTIFACTORY_CREDENTIALS_USR} -p ${PYPI_ARTIFACTORY_CREDENTIALS_PSW} dist/*
 
-deploy-doc: # Deploy the Sphinx docs
+deploy-doc: # Deploy documentation to shared server
 	@[ "${DOCS_ROOT_PATH}" ] && echo "" > /dev/null || ( echo "DOCS_ROOT_PATH is not set"; exit 1 )
 	mkdir -m 0775 -p ${DOCS_ROOT_PATH}/${PACKAGE_NAME}/${PACKAGE_VERSION}
 	cp -R ./output/docs_build/* ${DOCS_ROOT_PATH}/${PACKAGE_NAME}/${PACKAGE_VERSION}
 	chmod -R 0775 ${DOCS_ROOT_PATH}/${PACKAGE_NAME}/${PACKAGE_VERSION}
 	cd ${DOCS_ROOT_PATH}/${PACKAGE_NAME} && ln -nsFfv ${PACKAGE_VERSION} current
 
-clean: # Delete build artifacts and do any custom cleanup such as spinning down services
+clean: # Clean build artifacts and temporary files
 	@rm -rf format build-doc build-package integration .pytest_cache
 	@rm -rf dist output
 	$(shell find . -type f -name '*py[co]' -delete -o -type d -name __pycache__ -delete)
@@ -112,7 +115,7 @@ clean: # Delete build artifacts and do any custom cleanup such as spinning down 
 # Other/helper commands #
 #########################
 
-build-env: ## Create environment and install packages. Specify env name with CONDA_ENV_NAME. Default env name is PACKAGE_NAME_PYTHON_VERSION.
+build-env: # Create a new conda environment with name {PACKAGE_NAME}_py{PYTHON_VERSION} and install packages
 	make create-env CONDA_ENV_NAME=$(CONDA_ENV_NAME)
 	conda run -n $(CONDA_ENV_NAME) make install
 
@@ -124,16 +127,15 @@ install-upstream-deps: # Install upstream dependencies
 	@echo "----------------------------------------"
 	@sh $(UTILS_DIR)/resources/scripts/install_dependency_branch.sh $(DEPENDENCY_NAME) $(BRANCH_NAME) $(WORKFLOW)
 
-format: setup.py pyproject.toml $(MAKE_SOURCES) # Run the code formatter and import sorter
+format: setup.py pyproject.toml $(MAKE_SOURCES) # Format code (isort and black)
 	isort $(LOCATIONS)
 	black $(LOCATIONS)
 	@echo "Ignore, Created by Makefile, `date`" > $@
 
-manual-deploy: # Manual deploy to Artifactory
+manual-deploy: # Deploy package (build, tag, and deploy)
 	@[ "${PYPI_ARTIFACTORY_CREDENTIALS_USR}" ] && echo "" > /dev/null || ( echo "PYPI_ARTIFACTORY_CREDENTIALS_USR is not set, export using simsci artifactory credentials"; exit 1 )
 	@[ "${PYPI_ARTIFACTORY_CREDENTIALS_PSW}" ] && echo "" > /dev/null || ( echo "PYPI_ARTIFACTORY_CREDENTIALS_PSW is not set, export using simsci artifactory credentials"; exit 1 )
 	make build-env
 	conda run -n $(CONDA_ENV_NAME) make build-package
 	conda run -n $(CONDA_ENV_NAME) make tag-version
 	conda run -n $(CONDA_ENV_NAME) make deploy-package-artifactory
-
