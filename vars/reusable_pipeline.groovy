20652518e8582d1070dfb7ed122db4b27e8f968e
Steve Bachmeier <sbachmei@uw.edu>
2025-07-24 08:54:34 -0700

    transfom test_type request to actual target name

--- vars/reusable_pipeline.groovy
+++ vars/reusable_pipeline.groovy
@@ -39,6 +39,20 @@ def call(Map config = [:]){
   if (!test_types.every { ['all-tests', 'e2e', 'unit', 'integration'].contains(it) }) {
     throw new IllegalArgumentException("test_types must be a subset of ['all-tests', 'e2e', 'unit', 'integration']")
   }
+  
+  // Transform test type inputs to actual make test target names
+  def transformTestType(testType) {
+    switch(testType) {
+      case 'unit': return 'test-unit'
+      case 'integration': return 'test-integration'
+      case 'e2e': return 'test-e2e'
+      case 'all-tests': return 'test-all'
+      default: 
+        throw new IllegalArgumentException("Unknown test type: ${testType}")
+    }
+  }
+  test_types = test_types.collect { transformTestType(it) }
+  
   // Allow for building conda env on shared fs if required
   conda_env_name = config.use_shared_fs ? "${env.JOB_NAME.replaceAll('/', '-')}-${BUILD_NUMBER}" : "${env.JOB_NAME}-${BUILD_NUMBER}"
   conda_env_dir = config.use_shared_fs ? "/mnt/team/simulation_science/priv/engineering/tests/venv" : "/tmp"
