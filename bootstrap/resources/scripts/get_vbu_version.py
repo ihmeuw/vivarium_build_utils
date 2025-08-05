4ae1841cffa88c7dd016cc1edeab1e5d4c143bfe
Steve Bachmeier <sbachmei@uw.edu>
2025-08-05 12:11:20 -0700

    test branch

--- bootstrap/resources/scripts/get_vbu_version.py
+++ bootstrap/resources/scripts/get_vbu_version.py
@@ -112,10 +112,11 @@ def _extract_vbu_version(dry_run_output: str) -> str:
 def main() -> str:
     """Main function to orchestrate version resolution."""
 
-    python_version = _get_max_python_version()
-    dry_run_output = _run_pip_dry_run(python_version)
-    vbu_version = _extract_vbu_version(dry_run_output)
-
+    # FIXME PUT BACK AFTER TESTING
+    # python_version = _get_max_python_version()
+    # dry_run_output = _run_pip_dry_run(python_version)
+    # vbu_version = _extract_vbu_version(dry_run_output)
+    vbu_version = "sbachmei/hotfix/revert-v202"
     # NOTE: We must print the vbu_version for Jenkins to capture it!
     print(vbu_version)
     return vbu_version
