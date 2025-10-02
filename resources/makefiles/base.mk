495966eca7d44c358774f62300db833cb35285d2
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2025-10-02 15:41:56 -0700

    remove make sources; move phony tag declarations

--- resources/makefiles/base.mk
+++ resources/makefiles/base.mk
@@ -20,15 +20,7 @@ PYTHON_VERSION ?= 3.11
 CONDA_ENV_NAME ?= ${PACKAGE_NAME}_py${PYTHON_VERSION}
 CONDA_ENV_CREATION_FLAG = $(if $(CONDA_ENV_PATH),-p ${CONDA_ENV_PATH},-n ${CONDA_ENV_NAME})
 
-# These are the doc and source code files in this repo.
-# When one of these files changes, it means that Make targets need to run again.
-MAKE_SOURCES := $(shell find . -type d -name "*" ! -path "./.git*" ! -path "./.vscode" ! -path "./output" ! -path "./output/*" ! -path "./archive" ! -path "./dist" ! -path "./output/htmlcov*" ! -path "**/.pytest_cache*" ! -path "**/__pycache__" ! -path "./output/docs_build*" ! -path "./.pytype*" ! -path "." ! -path "./src/${PACKAGE_NAME}/legacy*" ! -path ./.history ! -path "./.history/*" ! -path "./src/${PACKAGE_NAME}.egg-info" ! -path ./.idea ! -path "./.idea/*" ) $(shell find . -name "setup.py" -o -name "pyproject.toml" | grep -v "./.git")
-
-# Phony targets don't produce artifacts.
-.PHONY: list debug sources
-.PHONY: create-env install lint mypy build-docs test-docs tag-version build-package deploy-package-artifactory deploy-docs clean
-.PHONY: check install-upstream-deps format manual-deploy-artifactory
-
+.PHONY: help
 help: # Curated help message 
 	(echo; \
 	echo "For Make's standard help, run 'make --help'."; \
@@ -39,7 +31,6 @@ help: # Curated help message
 	echo; \
 	echo "list       Print available Make targets"; \
 	echo "debug      Print debug information"; \
-	echo "sources    Print the source files that trigger Make targets"; \
 	echo; \
 	echo "=============="; \
 	echo "Helper targets"; \
@@ -80,6 +71,7 @@ help: # Curated help message
 # List of Make targets is generated dynamically. Note that to have a target show up in this
 # list, it must have an in-line comment starting with a '#' on the target definition,
 # e.g. some-target:  # this is the description for some-target
+.PHONY: list
 list: # Print available Make targets
 	@echo
 	@echo "Make targets:"
@@ -88,6 +80,7 @@ list: # Print available Make targets
 	done | column -t -s:
 	@echo
 
+.PHONY: debug
 debug: # Print debug information
 	@echo
 	@echo "'make' invoked with these environment variables:"
@@ -101,20 +94,12 @@ debug: # Print debug information
 	@echo
 	@echo "vivarium_build_utils version:     $(shell python -c "import importlib.metadata; print(importlib.metadata.version('vivarium_build_utils'))" 2>/dev/null || echo "unknown")"
 	@echo
-	@echo
-	@echo "For a list of all source files that trigger Make targets, run 'make sources'."
-	@echo
-
-sources: # Print the source files that trigger Make targets
-	@echo
-	@echo "Make sources:"
-	@echo "${MAKE_SOURCES}"
-	@echo
 
 #########################
 # Jenkins build targets #
 #########################
 
+.PHONY: create-env
 create-env: # Create a new conda environment
 # env name: {PACKAGE_NAME}_py{PYTHON_VERSION}.
 	conda create ${CONDA_ENV_CREATION_FLAG} python=${PYTHON_VERSION} --yes
@@ -122,6 +107,7 @@ create-env: # Create a new conda environment
 	@echo "Environment created ($(CONDA_ENV_NAME))"
 	@echo
 
+.PHONY: install
 install: ENV_REQS?=dev
 install: UV_FLAGS?=
 install: # Install package and dependencies
@@ -129,6 +115,7 @@ install: # Install package and dependencies
 	uv pip install --upgrade pip setuptools 
 	uv pip install -e .[${ENV_REQS}] --extra-index-url ${IHME_PYPI}simple/ --index-strategy unsafe-best-match ${UV_FLAGS}
 
+.PHONY: lint
 lint: # Check for formatting errors
 # NOTE: This is not actually running isort and black but rather just checking the --diffs.
 	@echo
@@ -136,32 +123,39 @@ lint: # Check for formatting errors
 	isort $(LOCATIONS) --check --verbose --only-modified --diff
 	black $(LOCATIONS) --check --diff
 
+.PHONY: mypy
 mypy: # Check for type hinting errors
 	mypy --config-file pyproject.toml .
 
 # test: test targets are defined in test.mk
 
-build-docs: $(MAKE_SOURCES) # Build documentation
+.PHONY: build-docs
+build-docs: # Build documentation
 	rm -rf docs/build/
 	$(MAKE) -C docs/ html SPHINXOPTS="-T -W --keep-going"
 
-test-docs: $(MAKE_SOURCES) # Test documentation examples
+.PHONY: test-docs
+test-docs: # Test documentation examples
 	$(MAKE) doctest -C docs/
 
+.PHONY: tag-version
 tag-version: # Tag current version and push to git
 	git tag -a "v${PACKAGE_VERSION}" -m "Tag automatically generated from Jenkins."
 	git push --tags
 
-build-package: $(MAKE_SOURCES) # Build pip wheel package
+.PHONY: build-package
+build-package: # Build pip wheel package
 	pip install build
 	python -m build
 
+.PHONY: deploy-package-artifactory
 deploy-package-artifactory: # Deploy the package to Artifactory
 	@[ "${PYPI_ARTIFACTORY_CREDENTIALS_USR}" ] && echo "" > /dev/null || ( echo "PYPI_ARTIFACTORY_CREDENTIALS_USR is not set, export using simsci artifactory credentials"; exit 1 )
 	@[ "${PYPI_ARTIFACTORY_CREDENTIALS_PSW}" ] && echo "" > /dev/null || ( echo "PYPI_ARTIFACTORY_CREDENTIALS_PSW is not set, export using simsci artifactory credentials"; exit 1 )
 	pip install twine
 	twine upload --repository-url ${IHME_PYPI} -u ${PYPI_ARTIFACTORY_CREDENTIALS_USR} -p ${PYPI_ARTIFACTORY_CREDENTIALS_PSW} dist/*
 
+.PHONY: deploy-docs
 deploy-docs: # Deploy documentation to shared server
 	@[ "${DOCS_ROOT_PATH}" ] && echo "" > /dev/null || ( echo "DOCS_ROOT_PATH is not set"; exit 1 )
 	mkdir -m 0775 -p ${DOCS_ROOT_PATH}/${PACKAGE_NAME}/${PACKAGE_VERSION}
@@ -169,6 +163,7 @@ deploy-docs: # Deploy documentation to shared server
 	chmod -R 0775 ${DOCS_ROOT_PATH}/${PACKAGE_NAME}/${PACKAGE_VERSION}
 	cd ${DOCS_ROOT_PATH}/${PACKAGE_NAME} && ln -nsFfv ${PACKAGE_VERSION} current
 
+.PHONY: clean
 clean: # Clean build artifacts and temporary files
 	@rm -rf format build-docs build-package integration .pytest_cache
 	@rm -rf dist output
@@ -178,6 +173,7 @@ clean: # Clean build artifacts and temporary files
 # Helper targets #
 ##################
 
+.PHONY: check
 check: # Run development checks
 	make lint
 # 	Run mypy if the py.typed file exists
@@ -201,6 +197,7 @@ check: # Run development checks
 	@echo
 	@echo "*** All checks passed successfully! ***"
 
+.PHONY: install-upstream-deps
 install-upstream-deps: # Install upstream dependencies
 	@echo "Contents of install_dependency_branch.sh"
 	@echo "----------------------------------------"
@@ -210,10 +207,12 @@ install-upstream-deps: # Install upstream dependencies
 	@sh $(UTILS_DIR)/resources/scripts/install_dependency_branch.sh $(DEPENDENCY_NAME) $(BRANCH_NAME) $(WORKFLOW)
 	@echo
 
-format: $(MAKE_SOURCES) # Format code (isort and black)
+.PHONY: format
+format: # Format code (isort and black)
 	isort $(LOCATIONS)
 	black $(LOCATIONS)
 
+.PHONY: manual-deploy-artifactory
 manual-deploy-artifactory: # Deploy package; only use if Jenkins deploy fails
 	@[ "${PYPI_ARTIFACTORY_CREDENTIALS_USR}" ] && echo "" > /dev/null || ( echo "PYPI_ARTIFACTORY_CREDENTIALS_USR is not set, export using simsci artifactory credentials"; exit 1 )
 	@[ "${PYPI_ARTIFACTORY_CREDENTIALS_PSW}" ] && echo "" > /dev/null || ( echo "PYPI_ARTIFACTORY_CREDENTIALS_PSW is not set, export using simsci artifactory credentials"; exit 1 )
