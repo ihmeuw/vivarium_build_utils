12f3bb8aa7869f8de7cfda17e1819e333bea31ae
Steve Bachmeier <sbachmei@uw.edu>
2026-04-24 10:31:23 -0600

    remove installDependencies

--- vars/build_stages.groovy
+++ vars/build_stages.groovy
@@ -88,7 +88,6 @@ def loadSharedFiles() {
             echo "Loading shared files into working directory: ${pwd()}"
             writeFile file: 'base.mk', text: libraryResource('makefiles/base.mk')
             writeFile file: 'test.mk', text: libraryResource('makefiles/test.mk')
-            writeFile file: 'install_dependency_branch.sh', text: libraryResource('scripts/install_dependency_branch.sh')
         }
     }
 }
